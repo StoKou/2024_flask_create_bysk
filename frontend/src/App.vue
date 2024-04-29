61a3f78b5ff5be57fdd4eb08cfb29ef6121e8f6a
StoKou <2781856148@qq.com>
2024-04-29 22:07:48 +0800

    npm pakcages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,44 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">首页</router-link>
-      <router-link to="/about">关于我们</router-link>
-      <router-link to="/contact">联系我们</router-link>
-      <router-link to="/expimage">实验图像</router-link>
-      <router-link to="/echartsdemo">测试模块</router-link>
-      <router-link to="/predictpage">预测</router-link>
-    </nav>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" router>
+      <el-menu-item index="/">首页</el-menu-item>
+      <el-submenu index="1">
+        <template slot="title">关于我们</template>
+        <el-menu-item index="/about">公司介绍</el-menu-item>
+        <el-menu-item index="/contact">联系我们</el-menu-item>
+        <!-- 如果有更多子菜单项，可以继续添加 -->
+      </el-submenu>
+      <el-menu-item index="/expimage">实验图像</el-menu-item>
+      <el-menu-item index="/echartsdemo">测试模块</el-menu-item>
+      <el-menu-item index="/predictpage">预测</el-menu-item>
+    </el-menu>
     <router-view />
   </div>
-</template>
+</template> 
+
+<script>
+import { Menu, MenuItem, Submenu } from 'element-ui';
+import { useRouter } from 'vue-router';
+
+export default {
+  components: {
+    [Menu.name]: Menu,
+    [MenuItem.name]: MenuItem,
+    [Submenu.name]: Submenu
+  },
+  setup() {
+    const router = useRouter();
+    const activeIndex = router.currentRoute.path;
+
+    return {
+      activeIndex
+    };
+  }
+};
+</script>
 
 <style scoped>
-/* 样式代码 */
-nav {
-  display: flex;
-  justify-content: space-around;
+.el-menu-demo {
   background-color: #f0f0f0;
-  padding: 10px;
-}
-
-router-link {
-  text-decoration: none;
-  color: #333;
-  font-weight: bold;
-  transition: color 0.3s;
-}
-
-router-link:hover {
-  color: #007bff;
 }
 </style>
